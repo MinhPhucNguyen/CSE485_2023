2b204247f70185867ed8bbd746d4ea4307e7e113
CaoBaLoi <2051063605@e.tlu.edu.vn>
2023-05-07 11:00:53 +0700

    Reload

--- BTTH01/StudentDAO.php
+++ BTTH01/StudentDAO.php
@@ -1,41 +1,82 @@
 <?php
-class StudentDAO {
-    private $studentList = array();
+require_once '../CSE485_2023/BTTH01/Student.php';
 
-    public function create(Student $student){
-        $this->studentList[] = $student;
+class StudentDAO
+{
+    private $studentsList = array();
+    public function create(Student $student)
+    {
+        $filename = '../students.csv';
+        $file = fopen($filename, 'a');
+        if ($file !== false) {
+            $array = array($student->getId(), $student->getName(), $student->getAge(), $student->getGrade());
+            fputcsv($file, $array);
+            fclose($file);
+            return true;
+        }
+        return false;
     }
-    public function read(string $path){
-        $file = fopen($path , 'r');
 
-        if(!$file){
-            echo 'File not found';
-            die;
-        }else{
-            global $studentList;
-            while (($data = fgetcsv($file)) !== false){
-                $student = new Student();
-                $student->setId($data[0]);
-                $student->setName($data[1]);
-                $student->setAge($data[2]);
-                $student->setGrade($data[3]);
-                $studentList = $student;
+    public function read()
+    {
+        $filename = '../students.csv';
+        if (file_exists($filename)) {
+            $file = fopen($filename, 'r');
+            if ($file) {
+                fgetcsv($file); //bỏ qua hàng tiêu đề
+                while (($row = fgetcsv($file)) !== false) {
+                    $student = new Student();
+                    $student->setId($row[0]);
+                    $student->setName($row[1]);
+                    $student->setAge($row[2]);
+                    $student->setGrade($row[3]);
+                    $this->studentsList[] = $student;
+                }
+            } else {
+                echo "Unable to open file";
             }
             fclose($file);
+        } else {
+            echo "File not found";
         }
-      
-    }
-    public function update(int $index ,Student $student){
-        $this->studentList[$index] = $student;
-    }
-    public function delete(int $index){
-        unset($this->studentList[$index]);
     }
 
-    public function getAll(){
-        
-        for($i = 0 ; $i < count($this->studentList) - 1 ; $i++){
-            echo $this->studentList[$i].'<br>';
-        } 
+    public function update(){
+        $filename = '../students.csv';
+        if (file_exists($filename)) {
+            $file = fopen($filename, 'r');
+            if ($file) {
+                fgetcsv($file); //bỏ qua hàng tiêu đề
+            }}
+    }
+
+    public function delete(){
+
+    }
+
+    public function getAll()
+    {
+        $filename = '../students.csv';
+        if (file_exists($filename)) {
+            $file = fopen($filename, 'r');
+            if ($file) {
+                fgetcsv($file); //bỏ qua hàng tiêu đề
+                while (($row = fgetcsv($file)) !== false) {
+                    $student = new Student();
+                    $student->setId($row[0]);
+                    $student->setName($row[1]);
+                    $student->setAge($row[2]);
+                    $student->setGrade($row[3]);
+                    $this->studentsList[] = $student;
+                }
+            } else {
+                echo "Unable to open file";
+            }
+            fclose($file);
+        } else {
+            echo "File not found";
+        }
+        return $this->studentsList;
     }
 }
+?>
